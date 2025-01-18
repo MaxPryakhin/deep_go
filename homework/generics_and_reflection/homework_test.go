649849eab9ec775b0f02ddb48538ed9bb84b7ad7
Max Pryakhin <maxim.k.pryakhin@gmail.com>
2025-01-18 17:43:40 +0300

    implement Serialize function

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"reflect"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,8 +19,48 @@ type Person struct {
 }
 
 func Serialize(person Person) string {
-	// need to implement
+	var sb strings.Builder
+	t := reflect.TypeOf(person)
+	v := reflect.ValueOf(person)
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get("properties")
+
+		if tag == "" {
+			continue
+		}
+
+		if strings.Contains(tag, ",omitempty") && isEmptyValue(v.Field(i)) {
+			continue
+		}
+
+		tag = strings.Split(tag, ",")[0]
+		sb.WriteString(tag + "=" + formatValue(v.Field(i)) + "\n")
+
+	}
+
+	return strings.TrimSuffix(sb.String(), "\n")
+}
+
+func formatValue(v reflect.Value) string {
+	switch v.Kind() {
+	case reflect.String:
+		return v.String()
+	case reflect.Bool:
+		return strconv.FormatBool(v.Bool())
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return strconv.FormatInt(v.Int(), 10)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return strconv.FormatUint(v.Uint(), 10)
+	case reflect.Float32, reflect.Float64:
+		return strconv.FormatFloat(v.Float(), 'f', -1, 64)
+	}
 	return ""
+}
+
+func isEmptyValue(v reflect.Value) bool {
+	return v.Interface() == reflect.Zero(v.Type()).Interface()
 }
 
 func TestSerialization(t *testing.T) {
